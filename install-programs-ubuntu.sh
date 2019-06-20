bc5d5eec23270cf4c2780afe4a2186e4e5510996
Peter Farr <Peter@PrismaPhonic.com>
2019-06-20 16:53:44 -0700

    Added tons of stuff for testing

--- install-programs-ubuntu.sh
+++ install-programs-ubuntu.sh
@@ -28,6 +28,18 @@ sudo apt install ranger caca-utils highlight atool w3m poppler-utils mediainfo
 # Install spacefm for bspwm
 sudo apt install spacefm
 
+# Install polybar dependencies
+sudo apt install cmake cmake-data libcairo2-dev libxcb1-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-image0-dev libxcb-randr0-dev libxcb-util0-dev libxcb-xkb-dev pkg-config python-xcbgen xcb-proto libxcb-xrm-dev i3-wm libasound2-dev libmpdclient-dev libiw-dev libcurl4-openssl-dev libpulse-dev libxcb-composite0-dev xcb libxcb-ewmh2
+
+# Install polybar
+git clone https://github.com/jaagr/polybar.git
+cd polybar && ./build.sh
+cd ../
+sudo rm -r polybar
+
+# Install sound tray for bspwm
+sudo apt install pasystray
+
 ## BSPWM INSTALL
 
 ./install-bspwm-ubuntu.sh
