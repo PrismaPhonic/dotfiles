da0f6695237a273d5d7ca977d6f151e237f1e749
Peter Farr <Peter@PrismaPhonic.com>
2019-06-23 10:07:37 -0700

    Updated ubuntu look script

--- ubuntu-gnome-look.sh
+++ ubuntu-gnome-look.sh
@@ -1,8 +1,9 @@
 # Set background to within
-gsettings set org.gnome.desktop.background picture-uri /usr/share/background/within.png
+gsettings set org.gnome.desktop.background picture-uri /usr/share/backgrounds/within.png
 
 # Install some cool themes
 git clone https://github.com/addy-dclxvi/gtk-theme-collections ~/.themes
+sudo apt install arc-theme
 
 # Install gnome shell extensions
 sudo apt install gnome-shell-extensions
@@ -13,21 +14,30 @@ sudo apt install chrome-gnome-shell
 
 # Get some swanky icons
 sudo add-apt-repository ppa:papirus/papirus
+sudo add-apt-repository ppa:numix/ppa
 sudo apt update
 sudo apt install papirus-icon-theme
+sudo apt install numix-icon-theme
+sudo apt install numix-icon-theme-circle
 
-# Install plank for that beautiful app bar look
-sudo apt install plank
-sudo cp /usr/share/applications/plank.desktop ~/.config/autostart/plank.desktop
-plank &
+# Install dash to dock
+mkdir dash-to-dock-temp
+cd dash-to-dock-temp
+git clone https://github.com/micheleg/dash-to-dock.git
+cd dash-to-dock
+make && make install
+dbus-send --type=method_call --print-reply --dest=org.gnome.Shell /org/gnome/Shell org.gnome.Shell.Eval string:'global.reexec_self()'
+gnome-shell-extension-tool -e dash-to-dock@micxgx.gmail.com
 
-# Permananetly hide the stock Ubuntu dock
-gsettings set org.gnome.shell.extensions.dash-to-dock autohide false
-gsettings set org.gnome.shell.extensions.dash-to-dock dock-fixed false
-gsettings set org.gnome.shell.extensions.dash-to-dock intellihide false
+# Set up dash to dock to look nice
+gsettings set org.gnome.shell.extensions.dash-to-dock extend-height false
+gsettings set org.gnome.shell.extensions.dash-to-dock dock-position BOTTOM
+gsettings set org.gnome.shell.extensions.dash-to-dock transparency-mode FIXED
+gsettings set org.gnome.shell.extensions.dash-to-dock dash-max-icon-size 64
+gsettings set org.gnome.shell.extensions.dash-to-dock unity-backlit-items true
 
 # Set GTK theme to Fantome
 gsettings set org.gnome.desktop.interface gtk-theme "Fantome"
 
-# Set icon theme to Papirus
-gsettings set org.gnome.desktop.interface icon-theme "Papirus"
+# Set icon theme to Numix-Circle
+gsettings set org.gnome.desktop.interface icon-theme "Numix-Circle"
